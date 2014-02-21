7a4f8240280980c14b7256f54b7771134be59ec6
Jessie Chapman <jessie.chapman@tufts.edu>
2014-02-20 21:23:50 -0500

    Lab 2

---
+++ reverselist.py
@@ -0,0 +1,9 @@
+# Jessie Chapman
+# Lab 2 - reverselist.py = prints a list in reverse order
+# Date: 2/20/2014
+
+list = ["apple", "banana", "carrot", "donkey kong"]
+list.reverse()
+for i, j in enumerate(list):
+        print j
+
